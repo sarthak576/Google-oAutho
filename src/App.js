e03ec1838b5ba63493f69234661594efb23527fa
sarthak576 <sarthakguptarjn@gmail.com>
2025-05-17 11:40:13 +0530

    EasyLoginV1.0 clientSecret app.js modified

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import Login from './components/login';
 import Logout from './components/logout';
 import { GoogleOAuthProvider } from "@react-oauth/google";
-
 const clientId = "6869876952-9ckskksajugc49bivcb2l3mlbf1v8bhu.apps.googleusercontent.com";
 // client Secret = "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"
 function App() {
@@ -12,8 +11,8 @@ function App() {
     <GoogleOAuthProvider clientId={clientId}>
       <div className="App">
         <h2>Google Login with OAuth</h2>
-        <Login />
-        <Logout />
+<Login/>
+<Logout/>
       </div>
     </GoogleOAuthProvider>
   );
